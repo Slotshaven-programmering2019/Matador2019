2eee034ab21a1c6dc924c8b5200b79864420456d
siinch <simon.ingemanns@protonmail.com>
2019-11-18 13:39:38 +0100

    kommentarer

--- Terninger.js
+++ Terninger.js
@@ -1,9 +1,13 @@
 class Terninger {
+
 	// eventuelt variabel til at se hvor mange gange der er slået 12 i træk
+	
 	constructor() {
 	
 	}
-
+	
+	// Simulerer kast med to seksidede terninger og returnerer antallet af øjne
+	// functionen er static og kan kaldes med ved at skrive "Terninger.Kast();"
 	static Kast () {
 		console.log("Terninger er kastet");
 		var terningEt = Math.ceil(Math.random() * 6);
