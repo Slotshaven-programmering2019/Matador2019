d71c506123020366ce67a1bf1e8eaa70def908da
casper siig <47445200+caspersiig@users.noreply.github.com>
2019-11-18 12:53:20 +0100

    tilføjede en start
    start knap der spørger om spiller tal
    samt starter main.js
    added alle .js tingende samt jquary

--- Main.js
+++ Main.js
@@ -1,1 +1,12 @@
-// instacere hele programmet
+function main(){
+var spillerantal = prompt("Hvor mange spillere");
+//lav spillerantal spillere
+//tegn alle tingende UI + Spillebræt + information omkring hvem der skal kaste først
+//få første spiller til at kaste
+/*flyt spiller og gør hvad feltet personen har landet på gør ved spilleren 
+(? felt få spilleren til at trække kortet, anden spillers felt få spilleren til at betale,osv.osv.)*/
+//giv spilleren de muligheder personen har til sidst (trade,køb huse,køb hoteller)
+
+}
+
+
