d79707b31eb838521cc7e398186dc5e89d2c9027
siinch <simon.ingemanns@protonmail.com>
2019-11-18 13:32:16 +0100

    terninger_returner_korrekt_vaerdi

--- Terninger.js
+++ Terninger.js
@@ -1,9 +1,15 @@
 class Terninger {
+	// eventuelt variabel til at se hvor mange gange der er slået 12 i træk
 	constructor() {
 	
 	}
 
 	static Kast () {
 		console.log("Terninger er kastet");
+		var terningEt = Math.ceil(Math.random() * 6);
+		var terningTo = Math.ceil(Math.random() * 6);
+		console.log(terningEt, terningTo);
+		console.log(terningEt + terningTo);
+		return terningEt + terningTo;
 	}
 }
