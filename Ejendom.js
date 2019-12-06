1bac84b1df583395457e50f6ef3258e2d7cbcab7
WeirdoCat <cykelmygmaerkeligmjc@gmail.com>
2019-12-06 15:10:52 +0100

    Begyndelse, og dantificerer

---
+++ Ejendom.js
@@ -0,0 +1,25 @@
+class Ejendom {
+    // 
+    constructor(pris,stolpris,uberstolpris,leje,leje1,leje2,leje3,leje4,lejeuber) {
+        this.pris = pris;
+        this.huspris;
+        this.lejesatser = [leje,leje1,leje2,leje3,leje4,lejeuber];
+
+        this.ejer;
+        this.stolpris=stolpris;
+        this.uberstolpris=uberstolpris;
+    }
+
+
+    pris() {
+         
+        return pris;
+    };
+
+    pantsæt(){
+        pantsat == true
+        ejer.penge += pris/2
+
+    }
+
+};
