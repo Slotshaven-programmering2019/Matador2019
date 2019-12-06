b4b32092c545ba7993eff365540df44c9429a859
nissemand243 <xander.s234@gmail.com>
2019-12-06 13:22:54 +0100

    beautify

--- Spiller.js
+++ Spiller.js
@@ -16,55 +16,55 @@ class Spiller {
 
     spillerUde = false;
 
-turStart(spiller){
-    if(spiller.vaerdi >=0){
-        spillerUde = true;
-        alert("Du er ude af spillet, fucking taber kælling")
-        if(spiller == [0]){
-            remove.spiller[0]
-        }else if(spiller == [1]){
-            remove.spiller[1]
-        }else if(spiller == [2]){
-            remove.spiller[2]
-        }else if(spiller == [3]){
-            remove.spiller[3]
+    turStart(spiller) {
+        if (spiller.vaerdi >= 0) {
+            spillerUde = true;
+            alert("Du er ude af spillet, fucking taber kælling")
+            if (spiller == [0]) {
+                remove.spiller[0]
+            } else if (spiller == [1]) {
+                remove.spiller[1]
+            } else if (spiller == [2]) {
+                remove.spiller[2]
+            } else if (spiller == [3]) {
+                remove.spiller[3]
+            }
         }
-    }
 
-    kastTerning(Terninger.kast());
+        kastTerning(Terninger.kast());
 
-    rykSpiller = function(position, terningSlag){
-        position = terningSlag + position;
-    }
-    
-    if(position == grund){
-        if(grundIkkeEjet){
-            alert("Vil du købe grunden?" + jaKnap + nejKnap)
-            jaKnap = createButton("Ja", jaTilKoeb);
-            nejKnap = createButton("nej", nejTilKoeb);
-            if(jaTilKoeb){
-                købEjendom();
-            }else if(nejTilKoeb){
-                if(spiller <=3){
-                    naesteTur(spiller ++);
-                }else if(spiller == 4){
-                    naesteTur(spiller == 0)
+        rykSpiller = function (position, terningSlag) {
+            position = terningSlag + position;
+        }
+
+        if (position == grund) {
+            if (grundIkkeEjet) {
+                alert("Vil du købe grunden?" + jaKnap + nejKnap)
+                jaKnap = createButton("Ja", jaTilKoeb);
+                nejKnap = createButton("nej", nejTilKoeb);
+                if (jaTilKoeb) {
+                    købEjendom();
+                } else if (nejTilKoeb) {
+                    if (spiller <= 3) {
+                        naesteTur(spiller++);
+                    } else if (spiller == 4) {
+                        naesteTur(spiller == 0)
+                    }
                 }
+            } else if (grundIkkeEjet == false) {
+                betalEjer(leje.grundIkkeEjet);
             }
-        }else if(grundIkkeEjet == false){
-            betalEjer(leje.grundIkkeEjet);
-        }
 
-        købEjendom = function(position){
+            købEjendom = function (position) {
 
-            if(penge > position.getParent().pris && position.getParent().ejer == bank){
-                
-                penge = penge-position.getparent().pris;
-                position.getParent().ejer = spiller;
-            }else if(penge < position.getParent().pris && position.getParent().ejer == bank){
-                alert('Du har ikke råd din samfundsnasserøv. Få dig et job');
-            }
-               /* switch (key) {
+                if (penge > position.getParent().pris && position.getParent().ejer == bank) {
+
+                    penge = penge - position.getparent().pris;
+                    position.getParent().ejer = spiller;
+                } else if (penge < position.getParent().pris && position.getParent().ejer == bank) {
+                    alert('Du har ikke råd din samfundsnasserøv. Få dig et job');
+                }
+                /* switch (key) {
                     case Proevlykken:
                         
                         break;
@@ -92,8 +92,7 @@ turStart(spiller){
                     
                         break;
                }*/
-        }    }
-}
-
- };
-
+            }
+        }
+    }
+};
