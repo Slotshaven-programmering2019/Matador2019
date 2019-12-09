1a25f6f0456e8513c97b2798ac231596d0947a07
nissemand243 <xander.s234@gmail.com>
2019-12-09 13:58:55 +0100

    flyttet kommentarer

--- Spiller.js
+++ Spiller.js
@@ -1,3 +1,6 @@
 class Spiller {
+ //giv spilleren de muligheder personen har til sidst (trade,køb huse,køb hoteller)
 
+     /*flyt spiller og gør hvad feltet personen har landet på gør ved spilleren 
+    (? felt få spilleren til at trække kortet, anden spillers felt få spilleren til at betale,osv.osv.)*/
 };
